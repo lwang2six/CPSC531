4f2e0a4046fa11585c6bbfd0861b64490ee059cf
Leo <leo@leo-Precision-WorkStation-390.(none)>
2012-04-19 23:22:29 -0700

    changes

--- settings.py
+++ settings.py
@@ -31,6 +31,8 @@ DATABASES = {
     }
 }
 
+STATIC_PREFIX = '/media'
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
@@ -56,12 +58,12 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = '%s/media'  % PROJECT_PATH
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '%/media'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
