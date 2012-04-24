2268c82f95f0d414cb39bc857ab7182188f478da
Leo <leo@leo-Precision-WorkStation-390.(none)>
2012-04-24 02:27:58 -0700

    update to apriori run

--- apriori/utils.py
+++ apriori/utils.py
@@ -48,6 +48,87 @@ def create_subset(person, record, fields):
 
     return True
 
+
+
+def create_subset2(person, record, search_fields, fields):
+    temp = set()
+    subset = set()
+    f_subset = set()
+
+    for i in fields:
+        field = i.split(':')[0]
+        s = f = ''
+        if field == 'age':
+            s = 'p.age=%s' % person.age
+            f = 'age:%s' % person.age
+        elif field == 'marital_status':
+            s = 'p.marital_status_id=%s' % person.marital_status.id
+            f = 'marital_status:%s' % person.marital_status.status
+        elif field == 'relationship':
+            s = 'p.relationship_id=%s' % person.relationship.id
+            f = 'relationship:%s' % person.relationship.type
+        elif field == 'sex':
+            s = 'p.sex="%s"' % person.sex
+            f = 'sex:%s' % person.get_sex_display()
+        elif field == 'race':
+            s = 'p.race_id=%s' % person.race.id
+            f = 'race:%s' % person.race.ethnicity
+        elif field == 'native_country':
+            s = 'p.native_country_id=%s' % person.native_country.id
+            f = 'native_country:%s' % person.native_country.name
+        elif field == 'education':
+            s = 'e.education_id=%s' % person.education.education.id
+            f = 'education:%s' % person.education.education.level
+        elif field == 'income':
+            s = 'w.income=%s' % int(person.work.income)
+            f = 'income:%s' % int(person.work.income)
+        elif field == 'occupation':
+            s = 'w.occupation_id=%s' % person.work.occupation.id
+            f = 'occupation:%s' % person.work.occupation.title
+        elif field == 'capital_gain':
+            s = 'w.capital_gain=%s' % person.work.capital_gain
+            f = 'capital_gain:%s' % person.work.capital_gain
+        elif field == 'capital_loss':
+            s = 'w.capital_loss=%s' % person.work.capital_loss
+            f = 'capital_loss:%s' % person.work.capital_loss
+        elif field == 'hours_per_week':
+            s = 'w.hours_per_week=%s' % person.work.hours_per_week
+            f = 'hours_per_week:%s' % person.work.hours_per_week
+        else: #'workclass':
+            s = 'w.workclass_id=%s' % person.work.workclass.id
+            f = 'workclass:%s' % person.work.workclass.title
+        
+        temp = set()
+        for k in f_subset:
+            temp.add('%s,%s' % (k, f))
+
+        f_subset.add('%s' % f)
+        f_subset = f_subset.union(temp)
+
+        temp = set()
+
+        for j in subset:
+            temp.add('%s and %s' % (j, s))
+        
+        subset.add('%s' % s)
+        subset = subset.union(temp)
+
+    count = 0
+    f_subset = list(f_subset)
+    for i in subset:
+        try:
+            sub = Subset.objects.get(record=record, fields=f_subset[count])
+        except:
+            raw_s = 'SELECT p.*, count(distinct p.census_person_id) AS total FROM census_person p, census_education e, census_work w where p.census_person_id=e.person_id and p.census_person_id=w.person_id and %s;' % i
+            p = Person.objects.raw(raw_s)
+            sub = Subset.objects.create(record=record, fields=f_subset[count], first_user=person, occurance=p[0].total, weight=len(f_subset[count].split(',')))
+
+            if sub.weight > record.weight:
+                sub.record.weight = sub.weight
+                sub.record.save()
+        count = count + 1
+
+
 def apriori(query_list, search_fields, fields_set):
 
     try:
@@ -69,9 +150,10 @@ def apriori(query_list, search_fields, fields_set):
         c = 0
         start = datetime.datetime.now()
         for person in census:
-            c = c + 1
-            for i in range(c, len(census)):
-                create_subset(person, record, get_fields(person, census[i], fields_set))
+            create_subset2(person, record, search_fields, fields_set)
+            #c = c + 1
+            #for i in range(c, len(census)):
+            #    create_subset(person, record, get_fields(person, census[i], fields_set))
                 
         end = datetime.datetime.now()
         record.run_time = (end-start).total_seconds()
